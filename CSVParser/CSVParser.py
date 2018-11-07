af655e92c9b37ea4ae6fab2b724555feb9353bbd
Michael <fitzichael@gmail.com>
2018-11-07 10:48:00 -0500

    QUICK PUSH

--- CSVParser/CSVParser.py
+++ CSVParser/CSVParser.py
@@ -22,7 +22,10 @@ for row in reader:
         row_to_add.append(row[i])
     spreadsheet.append(row_to_add)
     spreadsheet_iter += 1
+itr = 0;
 for row in spreadsheet[1:]:
+    print(round((itr/len(spreadsheet)*100), 2))
+    itr+=1
     search = convert_string_to_search(row[2])
     url = r'https://connectny.info/search~S0/?searchtype=l&searcharg=' + search
     tables = pd.read_html(url)
@@ -31,9 +34,12 @@ for row in spreadsheet[1:]:
         if tables[i][0][0] == "Library":
             library_table = tables[i]
     library_other_than_rensalaer = False
-    for library in library_table[0][1:]:
-        if (library != "Rensselaer"):
-            library_other_than_rensalaer = True
+    try:
+        for library in library_table[0][1:]:
+            if (library != "Rensselaer"):
+                library_other_than_rensalaer = True
+    except IndexError:
+        continue
     if library_other_than_rensalaer:
         row[1] = 'y'
     else:
